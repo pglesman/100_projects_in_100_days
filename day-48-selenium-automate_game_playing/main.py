b71b7e2441439cab261685e92ed3172ac8d42cd4
Patryk Glesman <pglesman@gmail.com>
2024-11-28 14:41:59 +0100

    After Day 49

--- day-48-selenium-automate_game_playing/main.py
+++ day-48-selenium-automate_game_playing/main.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
 import time
 
 web_link = 'https://orteil.dashnet.org/experiments/cookie/'
